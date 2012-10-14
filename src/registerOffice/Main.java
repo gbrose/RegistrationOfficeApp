84fe448ef71bde18d6d4e045dabf3bab69e5ad64
student <student@student-VirtualBox>
2012-10-14 13:41:50 +0200

    przygotowania do pracowni

--- src/registerOffice/Main.java
+++ src/registerOffice/Main.java
@@ -10,23 +10,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		Person p = new Person("Adam");
 		
-		PersonManager mgr= new PersonManager();
-		Condition condition = new GetByNameCondition("Adam");
-		mgr.save(p);
-		
-		Person result = mgr.get(new Condition<Person>()
-		{
-			@Override
-			public boolean check(Person obj) {
-				return obj.getName().equalsIgnoreCase("Adam");
-			}
-			
-		});
-		
-		System.out.println(result.getName());
-
 	}
 
 }
